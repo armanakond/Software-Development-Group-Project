2b23513877272b0aa96b6f6f03543f9944afb64e
armanakond <arman.akond01@gmail.com>
2025-04-30 00:28:09 +0100

    url fixes, for session page and voting page

--- skyhealth_app/voting/views.py
+++ skyhealth_app/voting/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def session_view(request):
+    return render(request, 'voting/session.html')
+
+
+def voting_view(request):
     return render(request, 'voting/voting.html')
+

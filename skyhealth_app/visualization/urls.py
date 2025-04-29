a233c88006de788e8df6f249fb94c19253ba07b3
MdHoque12 <w1972154@westminster.ac.uk>
2025-04-29 16:47:12 +0100

    Touched up on visualization files

--- skyhealth_app/visualization/urls.py
+++ skyhealth_app/visualization/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('trends/', views.trends_view, name='trends'),
-    path('teamsummary/', views.teamsummary_view, name='teamsummary'),
+    path('trends/', views.trends_view, name='trends'), # renders visualization/trends.html from templates
+    path('teamsummary/', views.teamsummary_view, name='teamsummary'), # renders visualization/teamsummary.html from templates
 ]
--- skyhealth_app/visualization/views.py
+++ skyhealth_app/visualization/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def trends_view(request):
     return render(request, 'visualization/trends.html')
-
+# presents the summaries and trend charts
 def teamsummary_view(request):
     return render(request, 'visualization/teamsummary.html')

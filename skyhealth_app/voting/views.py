75df520f561847855893243e32c3024eab96a83c
MdHoque12 <w1972154@westminster.ac.uk>
2025-04-30 19:01:03 +0100

    Added redirects from voting to dashboard

--- skyhealth_app/voting/views.py
+++ skyhealth_app/voting/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def session_view(request):
     return render(request, 'voting/session.html')
 
 
 def voting_view(request):
+    if request.method == "POST":
+
+        return redirect('dashboard')
+
     return render(request, 'voting/voting.html')
 

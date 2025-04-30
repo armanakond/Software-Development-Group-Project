91304e9e5ffe8d7fed8ebd442b0e8474c98306fe
MdHoque12 <w1972154@westminster.ac.uk>
2025-04-30 05:02:09 +0100

    ADDED NEW ADMIN BOX

--- skyhealth_app/accounts/admin.py
+++ skyhealth_app/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AccountEntry
 
 # Register your models here.
+@admin.register(AccountEntry)
+class AccountEntryAdmin(admin.ModelAdmin):
+    list_display = ('created',)
--- skyhealth_app/accounts/models.py
+++ skyhealth_app/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AccountEntry(models.Model):
+    """
+    A dummy model just to show the ACCOUNTS box.
+    Replace with real fields later.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Entry at {self.created}"

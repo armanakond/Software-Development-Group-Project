1e7c13f1508cce3282059ac949e9cfcbd00754e1
J-A39 <JunayedAhmed360@gmail.com>
2025-05-01 00:17:19 +0100

    fix

--- skyhealth_app/voting/models.py
+++ skyhealth_app/voting/models.py
@@ -10,32 +10,32 @@ class HealthVote(models.Model):
     session_date = models.CharField(max_length=100)
     team_name = models.CharField(max_length=100)
 
-    q1_vote = models.CharField(max_length=10 blank=True, null=True)
+    q1_vote = models.CharField(max_length=10, blank=True, null=True)
     q1_feedback = models.TextField(blank=True)
     q1_team_actions = models.TextField(blank=True)
     q1_org_solutions = models.TextField(blank=True)
 
-    q2_vote = models.CharField(max_length=10 blank=True, null=True)
+    q2_vote = models.CharField(max_length=10, blank=True, null=True)
     q2_feedback = models.TextField(blank=True)
     q2_team_actions = models.TextField(blank=True)
     q2_org_solutions = models.TextField(blank=True)
 
-    q3_vote = models.CharField(max_length=10 blank=True, null=True)
+    q3_vote = models.CharField(max_length=10, blank=True, null=True)
     q3_feedback = models.TextField(blank=True)
     q3_team_actions = models.TextField(blank=True)
     q3_org_solutions = models.TextField(blank=True)
 
-    q4_vote = models.CharField(max_length=10 blank=True, null=True)
+    q4_vote = models.CharField(max_length=10, blank=True, null=True)
     q4_feedback = models.TextField(blank=True)
     q4_team_actions = models.TextField(blank=True)
     q4_org_solutions = models.TextField(blank=True)
 
-    q5_vote = models.CharField(max_length=10 blank=True, null=True)
+    q5_vote = models.CharField(max_length=10, blank=True, null=True)
     q5_feedback = models.TextField(blank=True)
     q5_team_actions = models.TextField(blank=True)
     q5_org_solutions = models.TextField(blank=True)
 
-    q6_vote = models.CharField(max_length=10 blank=True, null=True)
+    q6_vote = models.CharField(max_length=10, blank=True, null=True)
     q6_feedback = models.TextField(blank=True)
     q6_team_actions = models.TextField(blank=True)
     q6_org_solutions = models.TextField(blank=True)

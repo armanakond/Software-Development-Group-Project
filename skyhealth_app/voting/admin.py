069505c6809a6ecdae0971e14f01bada61824b22
J-A39 <JunayedAhmed360@gmail.com>
2025-05-01 06:33:47 +0100

    furthers clarification through comments

--- skyhealth_app/voting/admin.py
+++ skyhealth_app/voting/admin.py
@@ -1,6 +1,7 @@
 #Author - Md Hoque
 #Co Author - Junayed Ahmed
 
+#All necessary imports required for admin.py and its functionalities
 from django.contrib import admin
 from .models import HealthVote
 from .models import Session, Team
@@ -14,11 +15,13 @@ class HealthVoteAdmin(admin.ModelAdmin):
     search_fields = ('user__username',)
 
 
+#admin.py code for Session Admin
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     list_display = ('name', 'date')
     ordering = ('date',)
 
+#admin.py code for Team Admin
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name',)
